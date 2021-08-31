c409ba0135e65330ac0b9082620018e282926354
BlackJackMC <duytu85343@gmail.com>
2021-08-31 16:28:46 +0700

    VT01.cpp update

---
+++ VT01.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int max(int arr[],int n)
+{
+    int max=arr[0];
+    for(int i = 0; i < n; i++)
+    {
+        if(arr[i]>max)
+        {
+            max=arr[i];
+        }
+    }
+    return max;
+}
+int main()
+{
+    int arr[10000000];
+    int n;
+    cin>>n;
+    for(int i=0; i<n; i++)
+    {
+        cin>>arr[i];
+    }
+    cout<<max(arr,n)<<endl;
+    return 0;
+}
